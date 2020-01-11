93902822485a9f778d09b7eda8f110bbdb71e1a8
Rossi Lorenzo <snowycoder@gmail.com>
2020-01-11 12:26:36 +0100

    Add channel info guessing from measure type
    Fix clippy warnings

--- src/web/db_helper.rs
+++ src/web/db_helper.rs
@@ -24,21 +24,79 @@ struct AutoChannelData {
     pub id_cnr: Option<String>,
 
     pub name: Option<String>,
+    pub measure_unit: Option<String>,
+}
+
+struct ChannelDetectedData {
+    pub measure_unit: String,
+    pub name: Option<String>,
+}
+
+fn guess_channel_info(m_type: &str) -> ChannelDetectedData {
+    if m_type.starts_with('T') {
+        let name = if m_type.starts_with("TSUP") {
+            Some("T. Superfice".to_string())
+        } else if m_type.starts_with("T_RUG") {
+            Some("T. Rugiada".to_string())
+        } else {
+            None
+        };
+        ChannelDetectedData {
+            measure_unit: "C°".to_string(),
+            name,
+        }
+    } else if m_type.starts_with("COND") {
+        ChannelDetectedData {
+            measure_unit: "C°".to_string(),
+            name: Some("T. Condensa".to_string()),
+        }
+    } else if m_type.starts_with("UR") {
+        ChannelDetectedData {
+            measure_unit: "%".to_string(),
+            name: Some("Umidità Relativa".to_string()),
+        }
+    } else if m_type.starts_with("RELAY") {
+        ChannelDetectedData {
+            measure_unit: "y/n".to_string(),
+            name: Some("Relay".to_string()),
+        }
+    } else if m_type.starts_with("CO2") {
+        ChannelDetectedData {
+            measure_unit: "PPM".to_string(),
+            name: Some("CO2".to_string()),
+        }
+    } else {
+        // Guessing failed
+        ChannelDetectedData {
+            measure_unit: m_type.to_string(),
+            name: None,
+        }
+    }
 }
 
 pub fn auto_create_site(site_id: IdType, cnr_id: &str, conn: &PgConnection, mysql_conn: &mysql::Pool) -> ServiceResult<()> {
     use crate::schema::sensor::dsl as sensor_dsl;
     use crate::schema::channel::dsl as channel_dsl;
 
-    let res = mysql_conn.prep_exec("SELECT DISTINCT idsensore, canale FROM (SELECT * FROM t_rilevamento_dati WHERE idsito = :site_id ORDER BY data DESC LIMIT 1000) AS tmp;", params!{
+    let res = mysql_conn.prep_exec("SELECT DISTINCT idsensore, canale, misura FROM (SELECT * FROM t_rilevamento_dati WHERE idsito = :site_id ORDER BY data DESC LIMIT 1000) AS tmp;", params!{
         "site_id" => cnr_id
     })?;
 
-    let mut sensor_to_channel: HashMap<String, Vec<String>> = HashMap::new();
+    struct ChannelData {
+        cnr_id: String,
+        measure_type: String,
+    }
+
+    let mut sensor_to_channel: HashMap<String, Vec<ChannelData>> = HashMap::new();
 
     for row in res {
-        let (sensor_id, channel_cnr_id) = mysql::from_row::<(String, String)>(row.unwrap());
-        sensor_to_channel.entry(sensor_id).or_insert_with(Vec::new).push(channel_cnr_id)
+        let (sensor_id, channel_cnr_id, channel_measure) = mysql::from_row::<(String, String, String)>(row.unwrap());
+        sensor_to_channel.entry(sensor_id).or_insert_with(Vec::new).push(
+            ChannelData {
+                cnr_id: channel_cnr_id,
+                measure_type: channel_measure,
+            }
+        )
     }
 
     for (id_cnr, mut channels) in sensor_to_channel.drain() {
@@ -54,10 +112,13 @@ pub fn auto_create_site(site_id: IdType, cnr_id: &str, conn: &PgConnection, mysq
             .get_result(conn)?;
 
         for x in channels.drain(..) {
+            let info = guess_channel_info(x.measure_type.as_str());
+
             let data = AutoChannelData {
                 sensor_id: id,
-                id_cnr: Some(x.clone()),
-                name: Some(x),
+                id_cnr: Some(x.cnr_id.clone()),
+                name: Some(info.name.unwrap_or(x.cnr_id)),
+                measure_unit: Some(info.measure_unit),
             };
             diesel::insert_into(channel_dsl::channel)
                 .values(&data)
@@ -71,18 +132,21 @@ pub fn auto_create_site(site_id: IdType, cnr_id: &str, conn: &PgConnection, mysq
 pub fn auto_create_sensor(site_cnr_id: &str, sensor_id: IdType, cnr_id: &str, conn: &PgConnection, mysql_conn: &mysql::Pool) -> ServiceResult<()> {
     use crate::schema::channel::dsl as channel_dsl;
 
-    let res = mysql_conn.prep_exec("SELECT DISTINCT canale FROM (SELECT * FROM t_rilevamento_dati WHERE idsito = :site_id AND idsensore = :sensor_id ORDER BY data DESC LIMIT 100) AS tmp;", params!{
+    let res = mysql_conn.prep_exec("SELECT DISTINCT canale, misura FROM (SELECT * FROM t_rilevamento_dati WHERE idsito = :site_id AND idsensore = :sensor_id ORDER BY data DESC LIMIT 100) AS tmp;", params!{
         "site_id" => site_cnr_id,
         "sensor_id" => cnr_id,
     })?;
 
     let channels: Vec<AutoChannelData> = res.map(|row| {
-        let cnr_id = mysql::from_row::<String>(row.unwrap());
+        let (cnr_id, measure_type) = mysql::from_row::<(String, String)>(row.unwrap());
+
+        let info = guess_channel_info(measure_type.as_str());
 
         AutoChannelData {
             sensor_id,
             id_cnr: Some(cnr_id.clone()),
-            name: Some(cnr_id),
+            name: Some(info.name.unwrap_or(cnr_id)),
+            measure_unit: Some(info.measure_unit),
         }
     }).collect();
 
